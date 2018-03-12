2a2b4df9fcc04151512583f4f8deb12ba344f597
tbq <tadbao@163.com>
2018-03-12 15:25:05 +0800

    可以dev d的 webview 优化

--- src/routers/router.js
+++ src/routers/router.js
@@ -3,18 +3,18 @@ import Vue from "vue"
 
 //使用Vue.use方法加载VueRouter模块
 Vue.use(VueRouter)
-import childrenView from "../components/tabbarcontainers/mine/ExpressEntry/ChildrenView.vue";
-import Pieces from "../components/tabbarcontainers/topieces/ToPieces.vue"
-import Sign from "../components/tabbarcontainers/sign/Sign.vue"
-import Detention from "../components/tabbarcontainers/detention/Detention.vue"
-import Mine from "../components/tabbarcontainers/mine/Mine.vue"
+// import childrenView from "../components/tabbarcontainers/mine/ExpressEntry/ChildrenView.vue";
+// import Pieces from "../components/tabbarcontainers/topieces/ToPieces.vue"
+// import Sign from "../components/tabbarcontainers/sign/Sign.vue"
+// import Detention from "../components/tabbarcontainers/detention/Detention.vue"
+// import Mine from "../components/tabbarcontainers/mine/Mine.vue"
 import Login from "../components/Login.vue"
-import Reset from "../components/comment/PasswordReset.vue"
-import Count from "../components/tabbarcontainers/mine/Count.vue"
-import ResetNext from "../components/comment/FindPasswordNext.vue"
-import DetailsEntry from "../components/tabbarcontainers/mine/ExpressEntry/DetailsEntry.vue";
-import ListEntry from "../components/tabbarcontainers/mine/ExpressEntry/ListEntry.vue";
-import QuryExpress from '../components/comment/QueryExpress.vue'
+// import Reset from "../components/comment/PasswordReset.vue"
+// import Count from "../components/tabbarcontainers/mine/Count.vue"
+// import ResetNext from "../components/comment/FindPasswordNext.vue"
+// import DetailsEntry from "../components/tabbarcontainers/mine/ExpressEntry/DetailsEntry.vue";
+// import ListEntry from "../components/tabbarcontainers/mine/ExpressEntry/ListEntry.vue";
+// import QuryExpress from '../components/comment/QueryExpress.vue'
 
 
 
@@ -23,71 +23,79 @@ export default new VueRouter({
     routes: [
             {
             path: "/",
-            component: Login
+			// component: resolve => require(["../components/Login.vue"], resolve)
+			component : Login
         },
         {
             path: "/login",
-            component: Login
+			// component: resolve => require(["../components/Login.vue"], resolve)
+			component : Login
+
         },
        
         {
             path: "/reset",
            
-            component: Reset
+			component: resolve => require(["../components/comment/PasswordReset.vue"], resolve)
+           
         },
         {
             path: "/next",
-            component: ResetNext
+			component: resolve => require(["../components/comment/FindPasswordNext.vue"], resolve)
+			
         },
         {
             path: "/pieces",
             meta: {
                 requireAuth: true, // 添加该字段，表示进入这个路由是需要登录的
             },
-            component: Pieces
+			component: resolve => require(["../components/tabbarcontainers/topieces/ToPieces.vue"], resolve)         
         },
         {
             path: "/sign",
             meta: {
                 requireAuth: true, // 添加该字段，表示进入这个路由是需要登录的
             },
-            component: Sign
+			component: resolve => require(["../components/tabbarcontainers/sign/Sign.vue"], resolve)
         },
         {
             path: "/detention",
             meta: {
                 requireAuth: true, // 添加该字段，表示进入这个路由是需要登录的
             },
-            component: Detention
+			component: resolve => require(["../components/tabbarcontainers/detention/Detention.vue"], resolve)
+           
         },
         {
             path: "/mine",
             meta: {
                 requireAuth: true, // 添加该字段，表示进入这个路由是需要登录的
             },
-			component: Mine,	
+			component: resolve => require(["../components/tabbarcontainers/mine/Mine.vue"], resolve)
+				
         },
         {
             path: "/count",
             meta: {
                 requireAuth: true, // 添加该字段，表示进入这个路由是需要登录的
             },
-            component: Count
+			component: resolve => require(["../components/tabbarcontainers/mine/Count.vue"], resolve)
+          
 		},
 		{
 			path: "/quryExpress",
 			meta: {
 				requireAuth: true, // 添加该字段，表示进入这个路由是需要登录的
 			},
-			component: QuryExpress
+			component: resolve => require(['../components/comment/QueryExpress.vue'], resolve)
+		
 		},
 		{
 			path: "/ExpressEntry",
 			meta: {
 				requireAuth: true, // 添加该字段，表示进入这个路由是需要登录的
 			},
-			component: childrenView,
-
+			component: resolve => require(["../components/tabbarcontainers/mine/ExpressEntry/ChildrenView.vue"], resolve),
 			children: [
 
 				{
@@ -95,14 +103,16 @@ export default new VueRouter({
 					meta: {
 						requireAuth: true, // 添加该字段，表示进入这个路由是需要登录的
 					},
-					component: DetailsEntry
+					component: resolve => require(["../components/tabbarcontainers/mine/ExpressEntry/DetailsEntry.vue"], resolve),
+					
 				},
 				{
 					path: "listEntry",
 					meta: {
 						requireAuth: true, // 添加该字段，表示进入这个路由是需要登录的
 					},
-					component: ListEntry
+					component: resolve => require(["../components/tabbarcontainers/mine/ExpressEntry/ListEntry.vue"], resolve),
+					
 				}
 			]
 		}		
--- webpack.config.pro.js
+++ webpack.config.pro.js
@@ -2,7 +2,9 @@ var path = require("path");
 const uglify = require('uglifyjs-webpack-plugin');
 //引入插件
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-
+// var CommonsChunkPlugin = new webpack.optimize.CommonsChunkPlugin('common')
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+// new ExtractTextPlugin ('style.css')
 //如果使用的是webpack.config.json的方式来配置webpack-dev-server的运行参数的
 //则需要引入webpack 在插件中新增一个 HotModuleReplacementPlugin
 var webpack = require("webpack")
@@ -10,18 +12,20 @@ var webpack = require("webpack")
 module.exports = {
 	entry : path.join(__dirname,'src/main.js'),
     output: {
-        path: path.join(__dirname, "dist"),
-        filename: "build.js"
+		path: path.join(__dirname, "/assets/js"),
+		filename: "build.js",
+		// publicPath: './',
     },
     module: {
         rules: [
-            {
-                //test指的就是在webpack处理文件的时候，用来匹配文件的后缀
-                test: /\.css$/,
-                //当文件后缀匹配之后，会调用这个数组中的加载器对文件进行打包操作
-                //调用加载器的顺序是从后向前的！
-                use: ['style-loader', 'css-loader']
-            },
+            // {
+            //     //test指的就是在webpack处理文件的时候，用来匹配文件的后缀
+            //     test: /\.css$/,
+            //     //当文件后缀匹配之后，会调用这个数组中的加载器对文件进行打包操作
+            //     //调用加载器的顺序是从后向前的！
+            //     use: ['style-loader', 'css-loader']
+			// },
+			{ test: /.css$/, use: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' }) },
             {
                 test: /\.less$/,
                 use: ['style-loader', 'css-loader', 'less-loader']
@@ -32,7 +36,7 @@ module.exports = {
             },
             {
                 test: /\.(jpg|png|gif|jpeg|svg|ttf|woff|woff2|eot)$/,
-                use: ['url-loader?limit=102400']
+                use: ['url-loader?limit=10240']
             },
             {
                 test: /\.js$/,
@@ -58,12 +62,28 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./index.html"
+			template : "./index.html",
+			// inject: 'body',
+			// filename: "../index.html"
         }),
+		new ExtractTextPlugin('../css/[name].css'),
 
-        new uglify(),
+		new webpack.HotModuleReplacementPlugin(),
+		new uglify(),
+		new webpack.optimize.CommonsChunkPlugin({
+			name: "commons",
+			// ( 公共chunk(commnons chunk) 的名称)
 
-        new webpack.HotModuleReplacementPlugin()
+			filename: "commons.js",
+			// ( 公共chunk 的文件名)
+
+			// minChunks: 3,
+			// (模块必须被3个 入口chunk 共享)
+
+			// chunks: ["pageA", "pageB"],
+			// (只使用这些 入口chunk)
+		})
+
     ],
     //下面这个属性是专门用来配置 webpack-dev-server的运行参数的
     devServer: {
@@ -74,7 +94,7 @@ module.exports = {
     },
     resolve: {
         alias: {
-            "vue": "vue/dist/vue.js"
+            "vue": "vue/dist/vue.min.js"
         }
     },
 }
